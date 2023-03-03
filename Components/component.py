3d08d18a9a32488d4e45a8969c776f4db2d86597
Kajo <james.ytedmonds@gmail.com>
2023-03-03 13:18:43 +0000

    New UI now changes component variables, used basic input validation

--- Components/component.py
+++ Components/component.py
@@ -24,7 +24,7 @@ class Component:
     y: float = 0
     z: float = 0
     xr: float = 0
-    yr: float = 0
+    # yr: float = 0
 
     def get_ui(self):
         q_list_widget_items = []
@@ -32,18 +32,22 @@ class Component:
         # component_name list element
         name_widget = self.make_config_widget("Name: ")
         name_textbox = QLineEdit()
+        name_textbox.setText(self.component_name)
+        name_textbox.editingFinished.connect(lambda: self.name_event_function(name_textbox.displayText()))
         name_widget.layout().addWidget(name_textbox)
         q_list_widget_items.append(name_widget)
 
         # X position list element
-        name_widget = self.make_config_widget("X: ")
-        name_textbox = QLineEdit()
-        name_widget.layout().addWidget(name_textbox)
-        q_list_widget_items.append(name_widget)
+        x_widget = self.make_config_widget("X: ")
+        x_textbox = QLineEdit()
+        x_textbox.setText(str(self.x))
+        x_textbox.editingFinished.connect(lambda: self.x_event_function(x_textbox.displayText()))
+        x_widget.layout().addWidget(x_textbox)
+        q_list_widget_items.append(x_widget)
 
         return q_list_widget_items
 
-    def make_config_widget(self, label_name:str):
+    def make_config_widget(self, label_name: str):
         widget = QWidget()
         layout = QHBoxLayout()
         list_item = QLabel(label_name)
@@ -54,3 +58,12 @@ class Component:
         widget.setLayout(layout)
 
         return widget
+
+    def name_event_function(self, widgetbox):
+        self.component_name = widgetbox
+
+    def x_event_function(self, widgetbox):
+        try:
+            self.x = float(widgetbox)
+        except ValueError:
+            print("You need to enter a number!")
