7d45ce0c727d5507d6fa2c06756a446774bc5dea
kajo <james.ytedmonds@gmail.com>
2023-04-27 23:42:42 +0100

    Work has begun on implementing the graphics view

--- UI_code/graphics_viewport.py
+++ UI_code/graphics_viewport.py
@@ -1,6 +1,15 @@
-from PySide6.QtWidgets import QGraphicsView
+from PySide6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsRectItem
+from PySide6.QtCore import QRectF
+from PySide6.QtGui import QTransform
+
 
 class GraphicsViewport(QGraphicsView):
 
     def __init__(self):
         super().__init__()
+
+        scene = QGraphicsScene()
+        test_rect = scene.addRect(QRectF(0, 0, 100, 100))
+
+        item = scene.itemAt(50, 50, QTransform())
+        self.setScene(scene)
